9787cd89fc747e03be9aa426488be95840e0037e
Kesavan <ykesan@gmail.com>
2016-02-26 15:35:42 +0530

    version changed.

--- src/test/java/org/wso2/carbon/connector/integration/test/instagram/instagramConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/instagram/instagramConnectorIntegrationTest.java
@@ -42,7 +42,7 @@ public class instagramConnectorIntegrationTest extends ConnectorIntegrationTestB
      */
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
-        init("instagram-connector-1.0.0");
+        init("instagram-connector-1.0.1-SNAPSHOT");
 
 	    esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
         esbRequestHeadersMap.put("Content-Type", "application/json");
