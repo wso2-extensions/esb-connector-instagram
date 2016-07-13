da727c33ad121ebbdbd4d1f8d8ff91d71f5b5a80
srajah.shakila@gmail.com <srajah.shakila@gmail.com>
2016-07-13 23:53:39 +0530

    After releasing the tag

--- src/test/java/org/wso2/carbon/connector/integration/test/instagram/instagramConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/instagram/instagramConnectorIntegrationTest.java
@@ -42,7 +42,7 @@ public class instagramConnectorIntegrationTest extends ConnectorIntegrationTestB
      */
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
-        init("instagram-connector-1.0.1");
+        init("instagram-connector-1.0.2-SNAPSHOT");
 
         esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
         esbRequestHeadersMap.put("Content-Type", "application/json");
